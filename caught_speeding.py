83a7c0a8aab32e9191bd23f1709fe01ad4861a11
shellyw19-meet <shellyw19@meet.mit.edu>
2017-07-26 18:23:22 +0300

    Stopping here

--- caught_speeding.py
+++ caught_speeding.py
@@ -1,26 +1,27 @@
 speed = int(input("What was your speed?"))
-birthday = imput("Is it your birthday?")
+birthday = input("Is it your birthday?")
 
 if speed < 31:
-    if birthday = no:
-    print("No ticket")
-    elif birthday = yes:
-    print("No ticket")
+    if birthday == "no":
+        print("No ticket")
+    elif birthday == "yes":
+        print("No ticket")
     
 elif speed > 30 and speed < 51:
-    if birthday = no:
-    print("Small ticket")
-    elif birthday = yes and speed < 36
-    print("No ticket")
+    if birthday == "no":
+        print("Small ticket")
+    elif birthday == "yes" and speed < 36:
+        print("No ticket")
           
 elif speed > 50:
-          if birthday = no:
-          print("Big ticket")
-          elif birthday = yes and speed < 56:
+          if birthday == "no":
+              print("Big ticke")
+          elif birthday == "yes" and speed < 56:
               print("Small ticket")
-              elif birthday = yes and speed > 55:
-                  print("Big ticket")
+          elif birthday == "yes" and speed > 55:
+              print("Big ticket")
 
+      
         
        
 
